eed2224441c45ca14491455dafae55b530b986bc
Priyanka-saw <priyankasaw51@gmail.com>
2025-09-30 14:15:53 +0530

    minor_changes

--- moveZeroEnd.java
+++ moveZeroEnd.java
@@ -1,21 +1,27 @@
+import java.util.Arrays;
+
 public class moveZeroEnd {
 
     public static void main(String[] args) {
-        int[] arr = { 1, 3, 5, 0, 7, 9, 4, 0, 1 };
+        int[] arr = { 1, 0, 5, 0, 7, 9, 4, 0, 1 };
         MoveEnd(arr);
+        System.out.print("Array after moving zeros to the end: " + Arrays.toString(arr));
     }
 
     static void MoveEnd(int[] arr) {
         int j = 0;
+        
+        // Move all non-zero elements to the front
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != 0) {
-
-                // swap
-                int temp = arr[i];
-                arr[j] = temp;
+                arr[j] = arr[i];
                 j++;
             }
         }
-
+        // Fill remaining positions with zeros
+        while (j < arr.length) {
+            arr[j] = 0;
+            j++;
+        }
     }
 }
